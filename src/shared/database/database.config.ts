48816abb8046de6810296887f82bd5561efba8f5
Alison Luiz <alison_luiz@outlook.com.br>
2023-11-25 17:15:29 -0300

    feat: config database

---
+++ src/shared/database/database.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/modules/users/entities/user.entity';
+
+export class DatabaseConfig {
+  static createTypeOrmOptions(
+    configService: ConfigService,
+  ): TypeOrmModuleOptions {
+    return {
+      url: configService.get('DATABASE_URL'),
+      ssl: false,
+      useUTC: true,
+      type: 'postgres',
+      synchronize: true,
+      entities: [User],
+    };
+  }
+}
