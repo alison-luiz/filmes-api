95eedbc2fbb9224fc8a0b9eadfe635ca366cb9e8
Alison Luiz <alison_luiz@outlook.com.br>
2023-11-26 16:34:32 -0300

    feat: update swagger partialtype

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,31 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiProperty({
-    example: 'Alison',
-    description: 'The first name of the user.',
-  })
-  @IsString()
-  @IsOptional()
-  first_name?: string;
-
-  @ApiProperty({
-    example: 'Silva',
-    description: 'The last name of the user.',
-  })
-  @IsString()
-  @IsOptional()
-  last_name?: string;
-
-  @ApiProperty({
-    example: 'alison_luiz@outlook.com.br',
-    description: 'The email address of the user.',
-  })
-  @IsEmail()
-  @IsString()
-  @IsOptional()
-  email?: string;
 }
